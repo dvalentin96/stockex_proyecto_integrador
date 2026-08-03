1bd10b93d4fe986165560fabfd78b1400ea4950e
dvalentin96 <wvalentind96@gmail.com>
2026-08-03 16:41:39 -0600

    Proveedor_Dao

--- dao/proveedor_dao.py
+++ dao/proveedor_dao.py
@@ -1,12 +1,14 @@
 from database.conexion import Conexion
 from models.proveedor import Proveedor
 
-class proveedorDAO:
+class ProveedorDAO:
 
     def obtener_todos(self):
         conexion = Conexion.obtener_conexion()
         cursor = conexion.cursor()
-        cursor.execute("SELECT * FROM proveedor")
+        conexion.rollback()
+
+        cursor.execute("SELECT * FROM proveedores")
         registros = cursor.fetchall()
 
         proveedores = []
@@ -25,12 +27,38 @@ class proveedorDAO:
         cursor.close()
         conexion.close()
         return proveedores
-    
+
+    def obtener_por_id(self, id_proveedor):
+        conexion = Conexion.obtener_conexion()
+        cursor = conexion.cursor()
+        conexion.rollback()
+
+        cursor.execute("SELECT * FROM proveedores WHERE id_proveedor = %s", (id_proveedor,))
+        registro = cursor.fetchone()
+
+        proveedor = None
+        if registro:
+            proveedor = Proveedor(
+                id_proveedor=registro[0],
+                nombre_proveedor=registro[1],
+                apellido_paterno=registro[2],
+                apellido_materno=registro[3],
+                telefono=registro[4],
+                email=registro[5],
+                direccion=registro[6]
+            )
+
+        cursor.close()
+        conexion.close()
+        return proveedor
+
     def insertar(self, proveedor):
         conexion = Conexion.obtener_conexion()
         cursor = conexion.cursor()
+        conexion.rollback()
+
         cursor.execute(
-            "INSERT INTO proveedor (nombre_proveedor, apellido_paterno, apellido_materno, telefono, email, direccion) VALUES (%s, %s, %s, %s, %s, %s)",
+            "INSERT INTO proveedores (nombre_proveedor, apellido_paterno, apellido_materno, telefono, email, direccion) VALUES (%s, %s, %s, %s, %s, %s)",
             (proveedor.nombre_proveedor, proveedor.apellido_paterno, proveedor.apellido_materno, proveedor.telefono, proveedor.email, proveedor.direccion)
         )
         conexion.commit()
@@ -40,8 +68,10 @@ class proveedorDAO:
     def actualizar(self, proveedor):
         conexion = Conexion.obtener_conexion()
         cursor = conexion.cursor()
+        conexion.rollback()
+
         cursor.execute(
-            "UPDATE proveedor SET nombre_proveedor = %s, apellido_paterno = %s, apellido_materno = %s, telefono = %s, email = %s, direccion = %s WHERE id_proveedor = %s",
+            "UPDATE proveedores SET nombre_proveedor = %s, apellido_paterno = %s, apellido_materno = %s, telefono = %s, email = %s, direccion = %s WHERE id_proveedor = %s",
             (proveedor.nombre_proveedor, proveedor.apellido_paterno, proveedor.apellido_materno, proveedor.telefono, proveedor.email, proveedor.direccion, proveedor.id_proveedor)
         )
         conexion.commit()
@@ -51,8 +81,9 @@ class proveedorDAO:
     def eliminar(self, id_proveedor):
         conexion = Conexion.obtener_conexion()
         cursor = conexion.cursor()
-        cursor.execute("DELETE FROM proveedor WHERE id_proveedor = %s", (id_proveedor,))
+        conexion.rollback()
+
+        cursor.execute("DELETE FROM proveedores WHERE id_proveedor = %s", (id_proveedor,))
         conexion.commit()
         cursor.close()
         conexion.close()
-
