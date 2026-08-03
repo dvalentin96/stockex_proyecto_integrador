a266e3059b2daf7f768c896ae4197fd4d6b8d581
dvalentin96 <wvalentind96@gmail.com>
2026-08-03 16:50:48 -0600

    Edit de venta.py

--- models/venta.py
+++ models/venta.py
@@ -1,91 +1,12 @@
-from database.conexion import Conexion
-from models.venta import Venta
+class Venta:
+    def __init__(self, id_venta, nombre_venta, usuario_id, ganancia, fecha, articulo):
+        self.id_venta = id_venta
+        self.nombre_venta = nombre_venta
+        self.usuario_id = usuario_id
+        self.ganancia = ganancia
+        self.fecha = fecha
+        self.articulo = articulo  # lista de id_producto
 
-class VentaDAO:
-
-    def obtener_todos(self):
-        conexion = Conexion.obtener_conexion()
-        cursor = conexion.cursor()
-        conexion.rollback()
-
-        cursor.execute("SELECT * FROM ventas")
-        registros = cursor.fetchall()
-
-        ventas = []
-        for registro in registros:
-            venta = Venta(
-                id_venta=registro[0],
-                nombre_venta=registro[1],
-                usuario_id=registro[2],
-                ganancia=registro[3],
-                fecha=registro[4],
-                articulo=registro[5]
-            )
-            ventas.append(venta)
-
-        cursor.close()
-        conexion.close()
-        return ventas
-
-    def obtener_por_id(self, id_venta):
-        conexion = Conexion.obtener_conexion()
-        cursor = conexion.cursor()
-        conexion.rollback()
-
-        cursor.execute("SELECT * FROM ventas WHERE id_venta = %s", (id_venta,))
-        registro = cursor.fetchone()
-
-        venta = None
-        if registro:
-            venta = Venta(
-                id_venta=registro[0],
-                nombre_venta=registro[1],
-                usuario_id=registro[2],
-                ganancia=registro[3],
-                fecha=registro[4],
-                articulo=registro[5]
-            )
-
-        cursor.close()
-        conexion.close()
-        return venta
-
-    def insertar(self, venta):
-        conexion = Conexion.obtener_conexion()
-        cursor = conexion.cursor()
-        conexion.rollback()
-
-        cursor.execute(
-            "INSERT INTO ventas (nombre_venta, usuario_id, ganancia, fecha, articulo) VALUES (%s, %s, %s, %s, %s)",
-            (venta.nombre_venta, venta.usuario_id, venta.ganancia, venta.fecha, venta.articulo)
-        )
-        conexion.commit()
-        cursor.close()
-        conexion.close()
-
-    def obtener_por_rango_fechas(self, fecha_inicio, fecha_fin):
-        conexion = Conexion.obtener_conexion()
-        cursor = conexion.cursor()
-        conexion.rollback()
-
-        cursor.execute(
-            "SELECT * FROM ventas WHERE fecha BETWEEN %s AND %s ORDER BY fecha",
-            (fecha_inicio, fecha_fin)
-        )
-        registros = cursor.fetchall()
-
-        ventas = []
-        for registro in registros:
-            venta = Venta(
-                id_venta=registro[0],
-                nombre_venta=registro[1],
-                usuario_id=registro[2],
-                ganancia=registro[3],
-                fecha=registro[4],
-                articulo=registro[5]
-            )
-            ventas.append(venta)
-
-        cursor.close()
-        conexion.close()
-        return ventas
+    def __str__(self):
+        return (f"ID: {self.id_venta}, Nombre: {self.nombre_venta}, Usuario ID: {self.usuario_id}, "
+                f"Ganancia: ${self.ganancia}, Fecha: {self.fecha}, Artículos: {self.articulo}")
